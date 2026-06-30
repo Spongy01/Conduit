3726d9adaff686aef139b5284e689801b8aa6e12
Ali Asgar Padaria <aliasgarpadaria002@gmail.com>
2026-06-29 23:09:06 -0700

    feat: created basic redis client with connect/disconnect and get/set methods

---
+++ gateway/core/redis_client.py
@@ -0,0 +1,51 @@
+import redis.asyncio as redis
+
+#create a base object
+class RedisClient:
+    def __init__(self, host: str = "localhost", port: int = 6379, db: int = 0):
+        self.host = host
+        self.port = port
+        self.db = db
+        self.client = None
+
+    def connect(self):
+        self.client = redis.Redis(host=self.host, port=self.port, db=self.db, decode_responses=True, max_connections=10)
+
+    async def disconnect(self):
+        if self.client:
+            await self.client.aclose()
+
+
+    def get_client(self):
+        if self.client is None:
+            raise Exception("Redis client is not connected. Call connect() first.")
+        return self.client
+    
+    async def set(self, key: str, value: str, expire: int = None):
+        client = self.get_client()
+        if expire:
+            await client.set(key, value, ex=expire)
+        else:
+            await client.set(key, value)
+
+    async def get(self, key: str):
+        client = self.get_client()
+        return await client.get(key)
+
+    # make hget set method and get method
+    async def hset(self, key:str, value: dict, expire: int = None):
+        client = self.get_client()
+        await client.hset(key, mapping=value)
+        if expire:
+            await client.expire(key, expire)
+    
+    async def hgetall(self, key:str):
+        client = self.get_client()
+        return await client.hgetall(key)
+
+    async def hget(self, key:str, field:str):
+        client = self.get_client()
+        return await client.hget(key, field)
+
+
+redis_client = RedisClient()
--- gateway/main.py
+++ gateway/main.py
@@ -3,14 +3,19 @@ from gateway.api.chat import router as chat_router
 from gateway.api.admin import router as admin_router
 from gateway.core.database import db
 from contextlib import asynccontextmanager
+from gateway.core.redis_client import redis_client
 
 @asynccontextmanager
 async def lifespan(app: FastAPI):
     # Startup code: Initialize the database connection pool
     await db.connect()
+    # Startup code: Initialize the Redis client
+    await redis_client.connect()
     yield
     # Shutdown code: Close the database connection pool
     await db.disconnect()
+    # Shutdown code: Close the Redis client
+    await redis_client.disconnect()
 
 
 app = FastAPI(lifespan=lifespan)
